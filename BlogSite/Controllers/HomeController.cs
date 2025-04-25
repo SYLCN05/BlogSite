f530410ed76dbf39c86d35ff8c6904da7629ddbe
SYLCN05 <Siyaryalcin25@gmail.com>
2025-04-25 22:17:39 +0200

    Added the view for a blog

--- BlogSite/Controllers/HomeController.cs
+++ BlogSite/Controllers/HomeController.cs
@@ -23,10 +23,9 @@ namespace BlogSite.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Blog()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
