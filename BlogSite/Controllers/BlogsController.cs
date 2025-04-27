1851a10e7b378a61a05b99c1564202c8e3010e68
SYLCN05 <Siyaryalcin25@gmail.com>
2025-04-27 22:01:15 +0200

    added razor page for blog and view for details
    also added some code in de Blogscontroller for the blogs index view

---
+++ BlogSite/Controllers/BlogsController.cs
@@ -0,0 +1,27 @@
+﻿
+using BlogSite.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public readonly BlogDbContext _context;
+        public BlogsController(BlogDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var blogs = await _context.Blogs.ToListAsync();
+            var blogs = await _context.Blogs.Where(b => b.Status == 1).ToListAsync();
+            return View(blogs);
+        }
+
+        public async Task<IActionResult> Details()
+        {
+            return View();
+        }
+    }
+}
