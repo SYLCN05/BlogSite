b2210562e9bc783e3dc7f774f9221090f9c1fc1f
SYLCN05 <Siyaryalcin25@gmail.com>
2025-04-27 22:00:04 +0200

    added blog data model with migrations
    also added a connection to the database and added the tables with entityframework core

---
+++ BlogSite/Data/BlogDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlogSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Data
+{
+    public class BlogDbContext:DbContext
+    {
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options ):base(options)
+        {
+            
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+    }
+}
